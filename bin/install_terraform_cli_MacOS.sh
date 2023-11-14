666b62ad00820c881c04ab0b739afb1529e663f3
Carlos Ramirez <carloslrm@gmail.com>
2023-11-14 15:52:48 -0600

    #8 adding local workspace setting

---
+++ bin/install_terraform_cli_MacOS.sh
@@ -0,0 +1,12 @@
+brew tap hashicorp/tap
+
+brew install hashicorp/tap/terraform
+
+brew update
+
+brew upgrade hashicorp/tap/terraform
+
+#Enable tab completion
+
+touch ~/.bashrc
+terraform -install-autocomplete
