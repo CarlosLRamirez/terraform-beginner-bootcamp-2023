eb3140687d83f5ccb0d5b66e46582bee4b62e207
Carlos Leonel Ramirez <carloslrm@gmail.com>
2023-11-16 17:05:19 -0600

    #16 install tf provider, create and destroy s3 bucket (#17)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.25.0"
+    }
   }
 }
 
@@ -11,9 +15,21 @@ provider "random" {
   # Configuration options
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 resource "random_string" "bucket_name" {
   length = 16
+  lower = true
+  upper = false
   special = false
+}
+
+
+#https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
