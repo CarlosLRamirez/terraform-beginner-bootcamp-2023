65806be2ee7914436b2e060b829c1c26b3fa101a
Carlos Leonel Ramirez <carloslrm@gmail.com>
2023-11-16 17:21:13 -0600

    #18 test s3 bucket creating on local environment (#19)

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ provider "aws" {
   # Configuration options
 }
 
+
 resource "random_string" "bucket_name" {
   length = 16
   lower = true
@@ -26,13 +27,17 @@ resource "random_string" "bucket_name" {
   special = false
 }
 
+locals {
+    bucket_name = join("-", ["mybucket", random_string.bucket_name.result])
+}
+
 
 #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
 resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+  bucket = local.bucket_name
 }
 
 output "random_bucket_name" {
-    value = random_string.bucket_name.result
+    value = local.bucket_name
 }
 
