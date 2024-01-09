f16d3898a580d4d6d3e4e6f5b0a6bbfe5887bdc7
Carlos Leonel Ramirez <carloslrm@gmail.com>
2024-01-09 01:08:24 +0000

    minute 36:29 tf plan error

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -5,6 +5,6 @@ output "s3_website_endpoint" {
       value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
 
-output "cloudfront_endpoint" {
-    value = aws_cloudfront_distribution.default.domain_name
-}
+#output "cloudfront_endpoint" {
+#    value = aws_cloudfront_distribution.default.domain_name
+#}
