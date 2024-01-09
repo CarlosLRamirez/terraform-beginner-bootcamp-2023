7cfec8e0715517582e0b7c9295b4c2a4897907e3
Carlos Leonel Ramirez <carloslrm@gmail.com>
2024-01-09 13:01:24 +0000

    test 3

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,11 @@ output "s3_website_endpoint" {
 #    description = "Domain name for the CloudFront Distribution"
 #    value = module.terrahouse_aws.cloudfront_endpoint
 #}
+
+locals {
+    root_path = path.root
+}
+
+output "root.path" {
+    value = local.root_path
+}
